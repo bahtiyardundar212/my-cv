e9fbe6a15e92dde8d673a23cf45597416f106716
bahtiyar212 <bahtiyardundar03@gmail.com>
2022-09-16 00:27:13 +0300

    ADD LANGUAGE

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const pdfBtn=document.getElementById("pdfBtn")
+const languageSelect=document.getElementById("language")
+
+const keys = ["personelDescription", "educationTitle"]
+
+const dictionary=[
+  {
+    id: "personelDescription",
+    tr: "merhaba ben bahtiyar dündar frontend developer olarak çalışıyorum.",
+    en: "hi, I'm bahtiyar dündar. I've been workng as a Frontend Developer for 1 year.",
+  },
+  {
+    id: "educationTitle",
+    tr: "EĞİTİM",
+    en: "EDUCATİON",
+  },
+  {
+    id: "experienceTitle",
+    tr: "DENEYİM",
+    en: "EXPERİENCES",
+  },
+] 
 
 pdfBtn.addEventListener("click", function(){
     const myCv=document.getElementById("myCv")
@@ -14,3 +35,21 @@ pdfBtn.addEventListener("click", function(){
       // New Promise-based usage:
       html2pdf().set(opt).from(myCv).save();  
 })
+
+languageSelect.addEventListener("change", function(event){
+  console.log(event.target.value);
+  
+})
+
+
+function translate(selectedLanguage){
+  for (let i = 0; i < dictionary.length; i++) {
+    const element=document.getElementById(dictionary[i].id)
+    if(selectedLanguage === "en"){
+      element.innerText = dictionary[i].en
+    }
+    if(selectedLanguage === "tr"){
+      element.innerText = dictionary[i].tr
+    }
+  }
+}
